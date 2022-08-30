52daec57c39d8725e77ea2f8ee7491575815c977
Anton <altay77@inbox.ru>
2022-08-31 00:08:31 +0700

    tasktree solution

---
+++ TaskTree/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите любое натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+Console.Write("Таблица кубов чисел от 1 до N: ");
+while (a <= n)
+{
+    Console.Write($" {a*a*a} ");
+    a++;
+}
