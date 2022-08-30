efe4675be50a9a460130f3e6215e28e5dea3fa32
Anton <altay77@inbox.ru>
2022-08-30 22:53:27 +0700

    taskone solution

---
+++ TaskOne/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите любое пятизначное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+ int num1 = num / 10000;
+ int num2 = num % 10;
+ if (num1 == num2)
+ {
+    num = num - (num1*10000);
+    num = num / 10;
+    num1 = num / 100;
+    num2 = num % 10;
+    if (num1 == num2) Console.Write("Это число палиндром");
+    else Console.Write ("Это число не палиндром");
+ }
+ else Console.Write("Это число не палиндром");
